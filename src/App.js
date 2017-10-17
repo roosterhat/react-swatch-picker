3bc84062fffca439592c2d3bb0760ef29bc6c92a
Alessandro Garzaro <mail@alessandrogarzaro.net>
2017-10-17 23:22:18 +0200

    Added icon button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import IconButton from 'material-ui/IconButton';
 import Paper from 'material-ui/Paper';
+import SvgIcon from 'material-ui/SvgIcon';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +10,12 @@ import './App.css';
 <g id="format-color-fill"><path d="M16.56 8.94L7.62 0 6.21 1.41l2.38 2.38-5.15 5.15c-.59.59-.59 1.54 0 2.12l5.5 5.5c.29.29.68.44 1.06.44s.77-.15 1.06-.44l5.5-5.5c.59-.58.59-1.53 0-2.12zM5.21 10L10 5.21 14.79 10H5.21zM19 11.5s-2 2.17-2 3.5c0 1.1.9 2 2 2s2-.9 2-2c0-1.33-2-3.5-2-3.5z"></path><path fill-opacity=".36" d="M0 20h24v4H0z"></path></g>
 </defs></svg>
 </iron-iconset-svg> */}
+
+const SwatchPickerIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M16.56 8.94L7.62 0 6.21 1.41l2.38 2.38-5.15 5.15c-.59.59-.59 1.54 0 2.12l5.5 5.5c.29.29.68.44 1.06.44s.77-.15 1.06-.44l5.5-5.5c.59-.58.59-1.53 0-2.12zM5.21 10L10 5.21 14.79 10H5.21zM19 11.5s-2 2.17-2 3.5c0 1.1.9 2 2 2s2-.9 2-2c0-1.33-2-3.5-2-3.5z"></path><path fill-opacity=".36" d="M0 20h24v4H0z"></path>
+  </SvgIcon>
+);
 
 class App extends Component {
   constructor(props) {
@@ -26,6 +34,9 @@ class App extends Component {
           <h1 className="App-title">Swatch Picker</h1>
         </header>
         <main className="App-main">
+          <IconButton>
+            <SwatchPickerIcon />
+          </IconButton>
           <Paper className="swatchPicker" elevation={4}>
             {
               this.state.colorList.map(function(color){
